9e544175e1aa2390fa5829bc10c700ddc75d1f6e
Mortemka <60519596+Mortemka@users.noreply.github.com>
2021-08-18 22:50:45 +0500

    update script

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,20 @@
+// Подключение по указанному url и получение ответа
 const getData = async url => {
   const response = await fetch(url)
   const data = await response.json()
   return data
 }
 
-// получаем данные
+// Получение данных
 getData('https://script.google.com/macros/s/AKfycbwnuav41ZzVpZhfNhu8jUJgVvMHf2Mm5svbxZ1ByyPBTwFFE2iPRG9mSZPHYjcwMED6GQ/exec')
   .then( data => {
-      // проверяем
-      console.log(data[0])
-    
+      // Проверка данных
+      console.log(data)
+      // Инициализация данных
+      var list_of_products = data[0];
+      var list_of_boxes = data[1];
   })
 
+
+  
  
