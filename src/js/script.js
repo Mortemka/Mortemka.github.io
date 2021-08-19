2bed28ea988b516b07bb3722d1b96a9a6a09035e
Mortemka <60519596+Mortemka@users.noreply.github.com>
2021-08-19 23:57:03 +0500

    preFinish

--- src/js/script.js
+++ src/js/script.js
@@ -36,9 +36,42 @@ function select_filling (list_of_products,list_of_boxes) {
           break;    
         }
       }
+      
+      var elem = document.querySelector(".boxCalculator__box");
+      var box_profile = [];
+      var box_profileSum = "";
+      var ext_box = [];
+      var box_ext_profile = [];
+      var box_ext_profileSum = [];
+
+      for(let j=0; j<box.length; j++){
+        if(box[j].flag == "Да"){
+          ext_box[j] = getBox(box[j].ext_id,list_of_boxes);
+        }
+        box_ext_profile[j] = [];
+        box_ext_profileSum[j] = "<br>"+ "Возможно комплектовать в другие коробки:" + "<br>";
+        for(let i=0; i<ext_box[j].length; i++){
+          let number = i+1;
+          box_ext_profile[j][i] = "Габариты " + number + "-й внешней коробки: " + ext_box[j][i].profile + " - " + 
+          "Количество изделий в коробке: " + box[j].quantity.split(";")[i];
+          if(i!=ext_box[j].length-1){        
+            box_ext_profile[j][i] = box_ext_profile[j][i] + "<br>";
+          }
+          box_ext_profileSum[j] = box_ext_profileSum[j] + box_ext_profile[j][i];
+        }
+
+        let number = j+1;
+        box_profile[j] = "Габариты " + number + "-й коробки: " + box[j].profile + " - " + 
+        "Количество изделий в коробке: " + product.quantity.split(";")[j] + box_ext_profileSum[j];
+        if(j!=box.length-1){        
+          box_profile[j] = box_profile[j] + "<br>";
+        }
+        box_profileSum = box_profileSum + box_profile[j];
+      }
        
-    var elem = document.querySelector(".boxCalculator__box");
-    elem.innerHTML = "Габариты коробки: " + box.profile + "\n" + "Количество изделий в коробке: " + product.quantity + ".";
+
+    document.querySelector(".boxCalculator__boxProfile").innerHTML = box_profileSum;
+        
     elem.style.display = "block";
 
     } else     {
@@ -51,10 +84,14 @@ function select_filling (list_of_products,list_of_boxes) {
 
   })
 function getBox(id_box,list_of_boxes){
+
+  var arr = id_box.split(";");
+  var box=[];
   for(let j = 0; j<list_of_boxes.length; j++) {
-    if(id_box == list_of_boxes[j].id) {
-      var box = list_of_boxes[j];
-      break;
+    for(let i=0; i<arr.length; i++){
+      if(arr[i] == list_of_boxes[j].id) {                
+        box[i] = list_of_boxes[j];        
+      }
     }
   }
   return box;
